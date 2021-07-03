ca129c8fbc16ce7986531068be7850269cfb93ba
Adam <azippor@gmail.com>
2021-07-03 16:47:56 +0200

    spacegun radius

--- Assets/Scripts/Serialized/Spacegun.cs
+++ Assets/Scripts/Serialized/Spacegun.cs
@@ -13,6 +13,7 @@ public class SerializedSpacegun
     public float position_x;
     public float position_y;
     public int initial_population;
+    public float radius = Game_Settings.DEFAULT_SPACEGUN_RADIUS;
 
     public int population_max;
 
