bc17e5b6ac09e9763b71965781de5ef513658f9f
bondadam <adam.bond@etu.unice.fr>
2021-07-11 18:28:43 +0200

    spaceload back

--- Assets/Scripts/UI/SpaceLoad.cs
+++ Assets/Scripts/UI/SpaceLoad.cs
@@ -8,14 +8,13 @@ public class SpaceLoad : MonoBehaviour
     private static SpaceLoad instance = null;
     void Awake()
     {
-        /*if (instance == null)
+        if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(this.gameObject);
             return;
         }
         Destroy(this.gameObject);
-        */
     }
 
     public static void switchColors(Background_Color color){
